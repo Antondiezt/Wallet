a79e3d250ce507431a8bf86417d95ab94e78064c
antondiezt <anton.dieztubet@gmail.com>
2020-08-13 19:23:53 +0200

    changes

--- .ipynb_checkpoints/Wallet-checkpoint.py
+++ .ipynb_checkpoints/Wallet-checkpoint.py
@@ -1,6 +1,14 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 4
-}
+from constants import *
+
+import subprocess
+
+
+command = './derive -g --mnemonic="stable morning essence hybrid energy version sunny keen paddle refuse box naive survey cousin interest" --cols=path,address,privkey,pubkey --coin=eth,btctest --numderive=5 --format=json'
+
+p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()
+
+print(output)
+
+import json
+coins = json.loads(output)
---
+++ constants.py
@@ -0,0 +1,5 @@
+BTC = 'btc'
+ETH = 'eth'
+BTCTEST = 'btc-test'
+
+mnemonic = os.getenv('MNEMONIC', 'stable morning essence hybrid energy version sunny keen paddle refuse box naive survey cousin interest')
